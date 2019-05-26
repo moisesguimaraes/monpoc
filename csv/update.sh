36f7291ee1f3f258ac8f70107f2003a64522661f
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2019-05-26 21:16:56 +0200

    monpoc-csv-generator

---
+++ csv/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+monpoc-csv-generator Abilities > abilities.csv
+monpoc-csv-generator Buildings > buildings.csv
+monpoc-csv-generator Units     > units.csv
+monpoc-csv-generator Monsters  > monsters.csv
--- update_csvs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p csv
-./data_to_csv.py Abilities > csv/abilities.csv
-./data_to_csv.py Buildings > csv/buildings.csv
-./data_to_csv.py Units     > csv/units.csv
-./data_to_csv.py Monsters  > csv/monsters.csv
